663a3a235f2c53ec3c0a6d72e591f6133ed1f5bc
Luan Camilo Nogueira <luancamilo313@gmail.com>
2026-02-09 19:09:16 -0300

    Init project

---
+++ src/main/java/net/feliny/bts_backend/BtsBackendApplication.java
@@ -0,0 +1,13 @@
+package net.feliny.bts_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/feliny/bts_backend/BtsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.feliny.bts_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
