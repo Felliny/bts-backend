1877a43ddf91089eb5068339fe8c665eedf5412e
Luan Camilo Nogueira <luancamilo313@gmail.com>
2026-02-24 15:38:10 -0300

    feat: ADD email and CPF verifications
    if email or CPF already exists
    
    Add new exception ResourceAlreadyExistsException

--- src/main/java/net/feliny/bts_backend/service/impl/UserServiceImpl.java
+++ src/main/java/net/feliny/bts_backend/service/impl/UserServiceImpl.java
@@ -3,12 +3,15 @@ package net.feliny.bts_backend.service.impl;
 import lombok.AllArgsConstructor;
 import net.feliny.bts_backend.dto.UserDto;
 import net.feliny.bts_backend.entity.User;
+import net.feliny.bts_backend.exception.ResourceAlreadyExistsException;
 import net.feliny.bts_backend.mapper.UserMapper;
 import net.feliny.bts_backend.repository.UserRepository;
 import net.feliny.bts_backend.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -18,6 +21,17 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public UserDto createUser(UserDto userDto) {
+
+        Optional<User> existingEmail = userRepository.findByEmail(userDto.getEmail());
+        if (existingEmail.isPresent()){
+            throw new ResourceAlreadyExistsException("Email already registered");
+        }
+
+        Optional<User> existingCpf = userRepository.findByCpf(userDto.getCpf());
+        if (existingCpf.isPresent()){
+            throw new ResourceAlreadyExistsException("CPF already registered");
+        }
+
 
         User user = UserMapper.mapToUser(userDto);
 
